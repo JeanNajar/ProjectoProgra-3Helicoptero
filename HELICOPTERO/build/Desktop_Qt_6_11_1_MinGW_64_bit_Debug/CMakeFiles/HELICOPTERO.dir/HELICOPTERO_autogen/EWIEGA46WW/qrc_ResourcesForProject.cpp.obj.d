a1f65ed3d7792376dd216812c2ab9d6cc27d355c
Gabriel <gpopoff504@gmail.com>
2026-09-07 23:24:38 -0600

    Remove tracked build directory and add .gitignore

--- HELICOPTERO/build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/HELICOPTERO.dir/HELICOPTERO_autogen/EWIEGA46WW/qrc_ResourcesForProject.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/HELICOPTERO.dir/HELICOPTERO_autogen/EWIEGA46WW/qrc_ResourcesForProject.cpp.obj: \
- C:\C++FolderPP\ProjectoProgra\#Helicoptero\HELICOPTERO\build\Desktop_Qt_6_11_1_MinGW_64_bit_Debug\HELICOPTERO_autogen\EWIEGA46WW\qrc_ResourcesForProject.cpp
